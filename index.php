50a3155e713f3eb9fea7dce27ffc010b18dd1fbb
DESKTOP-V9BOV2N\Thanh <buitanthanh211098@gmail.com>
2017-09-19 08:26:11 +0700

    tk khach hang

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 require 'cores/JATOVI_Controller.php';
 include_once BASEPATH.'/controllers/page.php';
+include_once BASEPATH.'/controllers/taikhoan/taikhoanadmin.php';
 
 $module='';
 if (isset($_GET['module'])) {
@@ -13,6 +14,9 @@ switch ($module) {
 		break;
 	case 'taikhoankhachhang':
 		$taikhoankhachhang->dieuhuong();
+		break;
+	case 'taikhoanadmin':
+		$taikhoanadmin->dieuhuong();
 		break;
 	default:
 		$page->index();
