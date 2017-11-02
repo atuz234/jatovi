ff2eac1d3c741a75f0fd633a58a75b2955e879ea
AnhTuan <atuz234@gmail.com>
2017-11-02 22:24:16 +0700

    giao dien trang chu admin

--- admin/views/admin/page/index.php
+++ admin/views/admin/page/index.php
@@ -1,26 +1,52 @@
 <?php include_once BASEPATH.'/controllers/admin/page.php';	 ?>
-<a href="<?=base_url."index.php?module=quanlydonhang"?>">
-<i  class="fa fa-floppy-o"></i> 
-<b><?= $sodh;?></b>
-<p>don hang</p>
-</a>
-<a href="<?=base_url."index.php?module=quanlytintuc"?>">
-<i  class="fa fa-floppy-o"></i> 
-<b><?= $sott;?></b>
-<p>tin tuc</p>
-</a>
-<a href="<?=base_url."index.php?module=quanlysanpham"?>">
-<i  class="fa fa-floppy-o"></i> 
-<b><?= $sosp;?></b>
-<p>san pham</p>
-</a>
-<a href="<?=base_url."index.php?module=taikhoanadmin"?>">
-<i  class="fa fa-floppy-o"></i> 
-<b><?= $soadmin;?></b>
-<p>admin</p>
-</a>
-<a href="<?=base_url."index.php?module=taikhoankhachhang"?>">
-<i  class="fa fa-floppy-o"></i> 
-<b><?= $sokh;?></b>
-<p>khach hang</p>
-</a>
+<h1>Trang quản trị</h1>
+<div style="border: solid 1px black"></div>
+<div class="container" style="font-size: 30px">
+	<div class="row">
+		<div class="col-md-4">
+			<a href="<?=base_url."index.php?module=quanlydonhang"?>">
+				<i  class="fa fa-file-o"></i> 
+				<b><?= $sodh;?></b>
+				<p>don hang</p>
+			</a>
+		</div>
+		<div class="col-md-4">
+			<a href="<?=base_url."index.php?module=quanlytintuc"?>">
+				<i  class="fa fa-newspaper-o"></i> 
+				<b><?= $sott;?></b>
+				<p>tin tuc</p>
+			</a>
+		</div>
+		<div class="col-md-4">
+			<a href="<?=base_url."index.php?module=quanlysanpham"?>">
+				<i  class="fa fa-archive"></i> 
+				<b><?= $sosp;?></b>
+				<p>san pham</p>
+			</a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<a href="<?=base_url."index.php?module=taikhoanadmin"?>">
+				<i  class="fa fa-user"></i> 
+				<b><?= $soadmin;?></b>
+				<p>admin</p>
+			</a>
+		</div>
+		<div class="col-md-4">
+			<a href="<?=base_url."index.php?module=taikhoankhachhang"?>">
+				<i  class="fa fa-users"></i> 
+				<b><?= $sokh;?></b>
+				<p>khach hang</p>
+			</a>
+		</div>
+		<div class="col-md-4">
+			
+		</div>
+	</div>
+</div>
+
+			
+
+		
+			
